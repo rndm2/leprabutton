4531251a57215520ae7f15a31084b4ed98408569
Konstantyn Maryanovsky <konstantyn.maryanovsky@nextiva.com>
2017-10-29 18:02:07 +0200

    Custom link script now support array of links

--- google-chrome/userscripts/customLink.js
+++ google-chrome/userscripts/customLink.js
@@ -5,13 +5,18 @@ userScripts.push({
 
   run: (lpr2data, settings) => {
     const handleNewHtml = function(e) {
+      const titles = settings['plugin[customLinkTitle]'].split(';');
+      const links = settings['plugin[customLinkLink]'].split(';');
+
       $('.b-aside_government', e ? e.target : document).forEach(item => {
-        const $clone = $(item).clone();
-        const $link = $clone.find('a');
-        $link.attr('href', settings['plugin[customLinkLink]']);
-        $link.text(settings['plugin[customLinkTitle]']);
-        $clone.attr('style', 'margin-top: -5px;');
-        $clone.insertAfter(item);
+        titles.forEach((title, index) => {
+          const $clone = $(item).clone();
+          const $link = $clone.find('a');
+          $link.attr('href', links[index]);
+          $link.text(title);
+          $clone.attr('style', 'margin-top: -5px;');
+          $clone.insertAfter(item);
+        });
       });
     };
 
